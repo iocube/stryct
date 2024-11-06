6ed5db7fa864cff5de27110945783dfd335d3702
iocube <kr.arsenov@gmail.com>
2024-11-06 14:26:17 +0200

    main.sh adjustments

--- main.sh
+++ main.sh
@@ -6,21 +6,29 @@ echo "This program accepts keypairs stored in the PKCS #8 syntax."
 #TODO: test whether provided keys are in pkcs8 or pkcs1, convert or exit accordingly
 
 
-function init_check{};
-function parse_config{};
+function init_check {
+    which ssh-keygen ; echo $?
+    which openssl ; echo $?
+}
+
+#function parse_config {}  #TODO: define config
+
 function loadkey {};
 
 function s_encrypt {
-    echo "Using key"
+    
 }
 
 function s_decrypt {
 
 }
-#Choose mode
+
+function choose_mode {
 echo "Choose mode:
 - 1) Encrypt
 - 2) Decrypt
+- 3) Start chat client mode
+$(if [[ $using_tmpkeypair -eq '1' ]]; then echo "- X) Use temporary keypair (auto-generate)" ; else echo "- X) Use existing keypair (currently using temporary) " ; fi )
 "
 read -p "Choice: " mode_choice
 case $mode_choice in 
@@ -34,5 +42,7 @@ case $mode_choice in
         echo "Invalid mode choice." ; exit 1
         ;;
 esac
+}
+
 # Choose function
 # encrypt or decrypt
